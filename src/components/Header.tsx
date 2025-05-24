d10a8374a86a70c2084ffa427446cb5b3b60c742
MarinaArrieta <arrietamarina12@gmail.com>
2025-05-24 17:30:28 -0300

    feat(header): make Header responsive and adjust layout for mobile and tablet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,57 @@
-import { Box } from "@chakra-ui/react"
+import { Box, HStack, Image, Link } from "@chakra-ui/react"
 
 
 const Header = () => {
-  return (
-    <Box color='brand.500'>
-      Vite + React
-    </Box>
-  )
+    return (
+        <HStack
+            justifyContent={{ base: "center", md: "space-around" }}
+            height="125px"
+            gap="0"
+        >
+            <Box
+                display='flex'
+                alignItems='center'
+                gap='20px'
+            >
+                <Image height="40px" src="./public/logo.png" alt="Fundación Viva" />
+                <Link
+                    color="accent.500"
+                    href="#"
+                    fontSize={{base: 'xs', md: 'md'}}
+                >
+                    Sitio institucional
+                </Link>
+            </Box>
+            <Box
+                display='flex'
+                alignItems='center'
+                gap='20px'
+            >
+                <Image
+                    height="40px"
+                    src="./public/fonselp-logo.png"
+                    alt="Fonselp"
+                    display={{ base: "none", sm: "block" }}
+                />
+                <Link
+                    color="accent.500"
+                    href="#"
+                    display={{ base: "none", sm: "inline" }}
+                    fontSize={{md: 'md'}}
+                >
+                    Registrate
+                </Link>
+                <Link
+                    color="accent.500"
+                    href="#"
+                    display={{ base: "none", sm: "inline" }}
+                    fontSize={{md: 'md'}}
+                >
+                    Iniciar Sesión
+                </Link>
+            </Box>
+        </HStack>
+    )
 }
 
 export default Header
