c8f05b0fa4e94c6909ddadeda2b96c35d1ef2d31
Natalia-Schwindt <naty8014@gmail.com>
2025-05-28 19:23:49 -0300

    feat(product-card): add ProductCard base component, import it in App and add image assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import ProductCard from "./components/ProductCard"
 
 function App() {
 
   return (
       <>
         <Header />
+        <ProductCard />
         <Footer />
       </>
   )
