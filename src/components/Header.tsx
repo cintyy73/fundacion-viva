8e90a625b62ef7fdfab23db4debb82da94a1566a
MarinaArrieta <arrietamarina12@gmail.com>
2025-05-29 14:37:45 -0300

    chore(theme): remove commented-out code from theme configuration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,11 +14,13 @@ const Header = () => {
                 alignItems='center'
                 gap='20px'
             >
-                <Image height="40px" src="./public/logo.png" alt="Fundación Viva" />
+                <Link href="https://www.fundacion-viva.org/" isExternal>
+                    <Image height="40px" src="./public/logo.png" alt="Fundación Viva" />
+                </Link>
                 <Link
                     color="secondary.default"
-                    href="#"
-                    fontSize={{base: 'xs', md: 'md'}}
+                    href="https://comprasocial.fonselp.org/"
+                    fontSize={{ base: 'xs', md: 'md' }}
                 >
                     Sitio institucional
                 </Link>
@@ -28,25 +30,27 @@ const Header = () => {
                 alignItems='center'
                 gap='20px'
             >
-                <Image
-                    height="40px"
-                    src="./public/fonselp-logo.png"
-                    alt="Fonselp"
-                    display={{ base: "none", sm: "block" }}
-                />
+                <Link href="https://comprasocial.fonselp.org/" isExternal>
+                    <Image
+                        height="40px"
+                        src="./public/fonselp-logo.png"
+                        alt="Fonselp"
+                        display={{ base: "none", sm: "block" }}
+                    />
+                </Link>
                 <Link
                     color="secondary.default"
-                    href="#"
+                    href="https://app.fonselp.com/login"
                     display={{ base: "none", sm: "inline" }}
-                    fontSize={{md: 'md'}}
+                    fontSize={{ md: 'md' }}
                 >
                     Registrate
                 </Link>
                 <Link
                     color="secondary.default"
-                    href="#"
+                    href="https://app.fonselp.com/login"
                     display={{ base: "none", sm: "inline" }}
-                    fontSize={{md: 'md'}}
+                    fontSize={{ md: 'md' }}
                 >
                     Iniciar Sesión
                 </Link>
