c7b0f5ab97e8b4da1e1c3b3fed37cd3cbd61f9c7
cintyy73 <cintiapaezdm@gmail.com>
2025-06-02 15:44:58 -0300

    feat(data): add ODS objects

---
+++ src/utils/constant.ts
@@ -0,0 +1,89 @@
+export const ods = [
+  {
+    id: 1,
+    name: "Fin de la pobreza (Objetivo 1)",
+    link: "https://www.un.org/sustainabledevelopment/es/poverty/",
+  },
+  {
+    id: 2,
+    name: "Hambre cero (Objetivo 2)",
+    link: "https://www.un.org/sustainabledevelopment/es/hunger/",
+  },
+  {
+    id: 3,
+    name: "Salud y bienestar (Objetivo 3)",
+    link: "https://www.un.org/sustainabledevelopment/es/health/",
+  },
+  {
+    id: 4,
+    name: "Educación de calidad (Objetivo 4)",
+    link: "https://www.un.org/sustainabledevelopment/es/education/",
+  },
+  {
+    id: 5,
+    name: "Igualdad de género y empoderamiento de la mujer (Objetivo 5)",
+    link: "https://www.un.org/sustainabledevelopment/es/gender-equality/",
+  },
+  {
+    id: 6,
+    name: "Agua limpia y saneamiento (Objetivo 6)",
+    link: "https://www.un.org/sustainabledevelopment/es/water-and-sanitation/",
+  },
+  {
+    id: 7,
+    name: "Energía asequible y no contaminante (Objetivo 7)",
+    link: "https://www.un.org/sustainabledevelopment/es/energy/",
+  },
+  {
+    id: 8,
+    name: "Trabajo decente y crecimiento económico (Objetivo 8)",
+    link: "https://www.un.org/sustainabledevelopment/es/economic-growth/",
+  },
+  {
+    id: 9,
+    name: "Industria, innovación e infraestructura (Objetivo 9)",
+    link: "https://www.un.org/sustainabledevelopment/es/infrastructure/",
+  },
+  {
+    id: 10,
+    name: "Reducción de las desigualdades entre países y dentro de ellos (Objetivo 10)",
+    link: "https://www.un.org/sustainabledevelopment/es/inequality/",
+  },
+  {
+    id: 11,
+    name: "Ciudades y comunidades sostenibles (Objetivo 11)",
+    link: "https://www.un.org/sustainabledevelopment/es/cities/",
+  },
+  {
+    id: 12,
+    name: "Producción y consumo responsables (Objetivo 12)",
+    link: "https://www.un.org/sustainabledevelopment/es/sustainable-consumption-production/",
+  },
+  {
+    id: 13,
+    name: "Acción por el clima (Objetivo 13)",
+    link: "https://www.un.org/sustainabledevelopment/es/climate-change-2/",
+  },
+  {
+    id: 14,
+    name: "Vida submarina (Objetivo 14)",
+    link: "https://www.un.org/sustainabledevelopment/es/oceans/",
+  },
+  {
+    id: 15,
+    name: "Vida de ecosistemas terrestres (Objetivo 15)",
+    link: "https://www.un.org/sustainabledevelopment/es/biodiversity/",
+  },
+  {
+    id: 16,
+    name: "Paz, justicia e instituciones sólidas (Objetivo 16)",
+    link: "https://www.un.org/sustainabledevelopment/es/peace-justice/",
+  },
+  {
+    id: 17,
+    name: "Alianzas para lograr los objetivos (Objetivo 17)",
+    link: "https://www.un.org/sustainabledevelopment/es/globalpartnerships/",
+  },
+];
+
+
