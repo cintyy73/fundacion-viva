adacbc7374bd936d4131987d69139c77512f7f9a
MarinaArrieta <arrietamarina12@gmail.com>
2025-06-07 17:18:51 -0300

    feat: add dynamic route for Details component (/details/:id)

--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -7,7 +7,7 @@ const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/details" element={<Details />} />
+      <Route path="/details/:id" element={<Details />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
