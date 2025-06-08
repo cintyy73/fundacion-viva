b383256d5efade6a6a922cf9a22a663d78c35700
Natalia-Schwindt <naty8014@gmail.com>
2025-06-08 10:00:39 -0300

    feat: add Banner component with autoplay image carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Routing from "./routes/Routing"
+import Banner from "./components/Banner"
 
 function App() {
 
   return (
     <>
       <Header />
+      <Banner />
       <Routing />
       <Footer />
     </>
---
+++ src/components/Banner.tsx
@@ -0,0 +1,80 @@
+import { Box, Image } from "@chakra-ui/react";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import { useEffect, useRef } from "react";
+
+const images = ["/fonselp1.png", "/fonselp2.png"];
+
+const Banner = () => {
+  const timer = useRef<ReturnType<typeof setInterval> | null>(null);
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    slides: {
+      origin: "center",
+      perView: 1,
+    },
+    created() {
+      startAutoSlide();
+    },
+    updated() {
+      startAutoSlide();
+    },
+    dragStarted() {
+      stopAutoSlide();
+    },
+    animationEnded() {
+      startAutoSlide();
+    },
+    dragEnded() {
+      startAutoSlide();
+    },
+  });
+  const startAutoSlide = () => {
+    stopAutoSlide();
+    timer.current = setInterval(() => {
+      instanceRef.current?.next();
+    }, 3000);
+  };
+
+  const stopAutoSlide = () => {
+    if (timer.current) clearInterval(timer.current);
+  };
+
+  useEffect(() => {
+    return () => {
+      stopAutoSlide();
+    };
+  }, []);
+  return (
+    <Box
+      ref={sliderRef}
+      className="keen-slider"
+      overflow="hidden"
+      w="full"
+      maxW={{ base: "80%", md: "80%", lg: "800px", xl: "1000px" }}
+      mx="auto"
+      h={{ base: "100px", md: "200px", xl: "250px" }}
+      m="10px auto"
+    >
+      {images.map((src, index) => (
+        <Box
+          className="keen-slider__slide"
+          key={index}
+          flex="none"
+          w="full"
+          h="full"
+        >
+          <Image
+            src={src}
+            alt={`slide-${index}}`}
+            objectFit="cover"
+            w="100%"
+            h="100%"
+          />
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default Banner;
