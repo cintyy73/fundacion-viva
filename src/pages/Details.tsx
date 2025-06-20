6f7a3a530131df9fde8f6369c88580b50150d1e5
MarinaArrieta <arrietamarina12@gmail.com>
2025-06-20 16:20:55 -0300

    refactor: renombrada la carpeta 'Map' a 'map' para mantener consistencia en la nomenclatura

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -21,7 +21,6 @@ import { IoLocationSharp } from 'react-icons/io5'
 import { LuHeartHandshake } from 'react-icons/lu'
 import { useNavigate, useParams } from 'react-router-dom'
 import { ods } from '@/utils/constant'
-// import { Map } from '../components/Map/index'
 import CardSkeletonDetail from '@/components/CardSkeletonDetail'
 import Map from '@/components/map/Map'
 
